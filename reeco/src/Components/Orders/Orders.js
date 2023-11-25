f72f0a495cd2c2a68a887692707718576f62fa13
Bharath <bk3604073@gmail.com>
2023-11-25 15:57:14 +0530

    second-commit

--- reeco/src/Components/Orders/Orders.js
+++ reeco/src/Components/Orders/Orders.js
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import LoadingOverlay from 'react-loading-overlay';
-import Swal from 'sweetalert2';
 
 import axios from 'axios';
 import { GoChevronRight } from "react-icons/go";
@@ -69,19 +67,6 @@ const StyledOrders = () => {
         { id: 6, label: 'Supplier', value: 'East Coast fruits & vegetables' },
     ];
 
-    const showSweetAlert = () => {
-        Swal.fire({
-            title: 'Loading...',
-            allowOutsideClick: false,
-            onBeforeOpen: () => {
-                Swal.showLoading();
-            },
-        });
-    };
-
-    const hideSweetAlert = () => {
-        Swal.close();
-    };
 
     // Function to add product with default values
     const onAddItem = async () => {
@@ -301,47 +286,8 @@ const StyledOrders = () => {
                             <LuPrinter style={{ color: `var(--background)` }} size={22} />
                         </Container>
                     </Container>
-                    {loading ? <LoadingOverlay
-                        active={loading}
-                        spinner
-                        text="Loading..."
-                        onClick={() => setLoading(false)}
-                        styles={{
-                            wrapper: {
-                                position: 'fixed',
-                                height: '100vh',
-                                width: '100vw',
-                                top: 0,
-                                left: 0,
-                                display: 'flex',
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                zIndex: 9999,
-                            },
-                            overlay: (base) => ({
-                                ...base,
-                                background: '#e8e8e8',
-                            }),
-                            content: (base) => ({
-                                ...base,
-                                color: 'var(--accent-background)',
-                            }),
-                            spinner: (base) => ({
-                                ...base,
-                                '& svg circle': {
-                                    stroke: 'var(--accent-background)', // Modify this line to change the spinner color
-                                },
-                            }),
-                        }}
-                        className="your-custom-class"
-                        fadeSpeed={1000}
-                    >
-                        {loading && showSweetAlert()}
-                        <div>
-                            <h1>Your Content Here</h1>
-                        </div>
-                        {loading && hideSweetAlert()}
-                    </LoadingOverlay> :<ProductsContainer>
+                    
+                    <ProductsContainer>
                         <OrderItemsTable >
                             <OrderItemsTableHead>
                                 <TableRow>
@@ -400,7 +346,7 @@ const StyledOrders = () => {
                                 ))}
                             </TableBody>
                         </OrderItemsTable>
-                    </ProductsContainer>}
+                    </ProductsContainer>
                 </OrderProductsWrapper>
             </OrderItemsContainer>
 
